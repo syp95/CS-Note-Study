2755eff88a184452294bf0492dd685a4c4c83969
syp <sy95_055@naver.com>
2022-11-07 18:47:46 +0900

    네트워크 기초, TCP/IP

---
+++ 1.design pattern/7.revealing module.js
@@ -0,0 +1,11 @@
+const pukuba = (() => {
+    const a = 1;
+    const b = () => 2;
+    const public = {
+        c: 2,
+        d: () => 3,
+    };
+    return public;
+})();
+console.log(pukuba);
+console.log(pukuba.a);
